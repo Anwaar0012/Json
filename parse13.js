97a9c5f337dcc9c96e0460aadc4b4f57a4faa868
Muhammad Anwaar <110621511+Anwaar0012@users.noreply.github.com>
2022-10-22 16:33:57 +0500

    Add files via upload

---
+++ parse13.js
@@ -0,0 +1,13 @@
+// when we receive data from web It is in string formate
+// and we parse the data into JavaScript Object with below method
+// JSON.parse(data)
+
+// Example 
+// var obj=JSON.parse('{"name":"anwaar","age":36}')
+
+// Excepption 
+// Date Object and function are not allowed in JSON
+// if you include a date or a function write it in string
+// YOu can convert it back into a date object and or function later
+
+
